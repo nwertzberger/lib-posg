6eeea6a6cafffe0f085c2d3d5df999e2a43e79d9
nwertzberger <wertnick@gmail.com>
2013-03-30 15:35:28 -0500

    Adding basic projet setup.

---
+++ src/main/java/com/ideaheap/libposg/App.java
@@ -0,0 +1,15 @@
+package com.ideaheap.libposg;
+
+/**
+ * Programmer: Nicholas Wertzberger
+ *
+ * Warm-up
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/groovy/com/ideaheap/libposg/AppTest.groovy
@@ -0,0 +1,15 @@
+package com.ideaheap.libposg
+
+import org.junit.Test
+
+/**
+ * User: nwertzberger
+ * Date: 3/30/13
+ * Time: 3:28 PM
+ */
+class AppTest {
+    @Test
+    void testStuff() {
+        assert true
+    }
+}
